28054e68c15fe63788412dda832362f2f3a049c1
Ben Aldrich <vrecan@gmail.com>
2014-08-06 21:44:35 -0600

    fix test

--- elasticsearch/elasticsearch.go
+++ elasticsearch/elasticsearch.go
@@ -1,5 +1,6 @@
 package elasticsearch
 
+//elasticsearch wrapper class
 import (
 	// "errors"
 	. "github.com/CapillarySoftware/gomasticate/stomach"
@@ -10,12 +11,14 @@ type Elasticsearch struct {
 	c *es.Conn
 }
 
+//Connect to elasticsearch
 func (this *Elasticsearch) Connect(url string) (err error) {
 	this.c = es.NewConn()
 	this.c.Domain = url
 	return
 }
 
+//Index documents into elasticsearch
 func (this *Elasticsearch) IndexDocument(index string, indexType string, id string, doc Document) (err error) {
 	_, err = this.c.Index(index, indexType, id, nil, doc)
 	this.c.Flush()
