0c14a4aa23a35a530f9565c34da3c0bae01e89b1
Ben Aldrich <vrecan@gmail.com>
2014-08-20 21:43:56 -0600

    fix indexs

--- chew/chew.go
+++ chew/chew.go
@@ -3,10 +3,12 @@ package chew
 //Chew is where we transform the data.
 
 import (
+	// "fmt"
 	"github.com/CapillarySoftware/goforward/messaging"
 	rep "github.com/CapillarySoftware/goreport"
 	log "github.com/cihub/seelog"
 	"sync"
+	"time"
 )
 
 func Chew(chewChan <-chan *messaging.Food, swallowChan chan *messaging.Food, wg *sync.WaitGroup) {
@@ -17,6 +19,14 @@ func Chew(chewChan <-chan *messaging.Food, swallowChan chan *messaging.Food, wg 
 	for msg := range chewChan {
 		if nil != msg {
 			//parsing work here probably change what our message type looks like when swallowed
+
+			date := time.Unix(0, msg.GetTimeNano()).UTC()
+			fmtDate := date.Format("2006-01-02")
+			indexType := "all"
+			customerId := "id" //should exist eventually
+			index := "documents-" + fmtDate + "-" + customerId
+			msg.Index = &index
+			msg.IndexType = &indexType
 			r.AddStatWIndex("chew", 1, "good")
 			swallowChan <- msg
 		}
